aa2eba6028fc1ee4251990dc55ef7e90b14cb98a
guille278 <guillermoal278@gmail.com>
2022-10-27 22:44:39 -0500

    fin

--- app/src/main/java/com/example/practica5/MainActivity.java
+++ app/src/main/java/com/example/practica5/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.practica5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,19 +24,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SimpleDateFormat fecha = new SimpleDateFormat("dd/MM/yyyy");
-        SimpleDateFormat hora = new SimpleDateFormat("hh:mma");
         btnFecha = findViewById(R.id.gFecha);
         btnHora = findViewById(R.id.gHora);
         etFecha = findViewById(R.id.et_fecha);
         etHora = findViewById(R.id.et_hora);
 
         btnFecha.setOnClickListener(listener -> {
-            etFecha.setText(fecha.format(new Date()));
+            Calendar c = Calendar.getInstance();
+            int dia = c.get(Calendar.DAY_OF_MONTH);
+            int mes = c.get(Calendar.MONTH);
+            int anio = c.get(Calendar.YEAR);
+            new DatePickerDialog(this,
+                    (datePicker, i, i1, i2) -> {
+                        etFecha.setText(i + " / " + i1 + " / " + i2);
+                    },
+                    anio,
+                    mes,
+                    dia).show();
+
         });
 
         btnHora.setOnClickListener(listener -> {
-            etHora.setText(hora.format(new Date()));
+            Calendar c = Calendar.getInstance();
+            int hora = c.get(Calendar.HOUR);
+            int min = c.get(Calendar.MINUTE);
+            new TimePickerDialog(this, (timePicker, i, i1) -> {
+                etHora.setText(i + ":" + i1);
+            }, hora, min, false).show();
         });
 
     }
